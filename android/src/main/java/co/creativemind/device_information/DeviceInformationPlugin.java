7d77841f269f07f5a08fb564d9f9eea59843bc98
Bash <yesufubashir@gmail.com>
2025-09-10 16:13:06 +0100

    add v2 support

--- android/src/main/java/co/creativemind/device_information/DeviceInformationPlugin.java
+++ android/src/main/java/co/creativemind/device_information/DeviceInformationPlugin.java
@@ -24,10 +24,9 @@ import io.flutter.plugin.common.MethodCall;
 import io.flutter.plugin.common.MethodChannel;
 import io.flutter.plugin.common.MethodChannel.MethodCallHandler;
 import io.flutter.plugin.common.MethodChannel.Result;
-import io.flutter.plugin.common.PluginRegistry.Registrar;
 
 /** DeviceInformationPlugin */
-public class DeviceInformationPlugin implements FlutterPlugin, MethodCallHandler, ActivityAware {
+public class DeviceInformationPlugin implements FlutterPlugin, MethodChannel.MethodCallHandler, ActivityAware {
   /// The MethodChannel that will the communication between Flutter and native Android
   ///
   /// This local reference serves to register the plugin with the Flutter Engine and unregister it
